fe84a128edf4c9d573a6141dbd482bb06fed9f02
Park YuongHo <hexdrinker2020@gmail.com>
2024-07-17 18:30:52 +0900

    feat: create server modal

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -23,9 +23,9 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { useEffect, useState } from 'react'
 import FileUpload from '@/components/file-upload'
 import { useRouter } from 'next/navigation'
+import { useModal } from '@/hooks/use-modal-store'
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -36,13 +36,11 @@ const formSchema = z.object({
   }),
 })
 
-const InitialModal = () => {
-  const [isMounted, setIsMounted] = useState(false)
+export const CreateServerModal = () => {
+  const { isOpen, onClose, type } = useModal()
   const router = useRouter()
 
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
+  const isModalOpen = isOpen && type === 'createServer'
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -59,18 +57,22 @@ const InitialModal = () => {
       await axios.post('/api/servers', values)
       form.reset()
       router.refresh()
-      window.location.reload()
+      onClose()
     } catch (error) {
       console.log(error)
     }
   }
 
-  if (!isMounted) {
-    return null
+  const handleClose = () => {
+    form.reset()
+    onClose()
   }
 
   return (
-    <Dialog open>
+    <Dialog
+      open={isModalOpen}
+      onOpenChange={handleClose}
+    >
       <DialogContent className='bg-white text-black p-0 overflow-hidden'>
         <DialogHeader className='pt-8 px-5'>
           <DialogTitle className='text-2xl text-center text-bold'>
@@ -140,5 +142,3 @@ const InitialModal = () => {
     </Dialog>
   )
 }
-
-export default InitialModal
