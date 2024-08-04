7631ce1fae769ed4b1281b53b0de2fda2830cb7f
Park YuongHo <hexdrinker2020@gmail.com>
2024-08-05 00:22:07 +0900

    fix: fix typo error

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -57,7 +57,7 @@ const ServerSearch = ({ data }: ServerSearchProps) => {
     }
 
     if (type === 'channel') {
-      return router.push(`/servers/${params.serverId}/channels/${id}`)
+      return router.push(`/servers/${params?.serverId}/channels/${id}`)
     }
   }
 
